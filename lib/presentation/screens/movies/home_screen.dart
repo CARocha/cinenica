04f0213ab22e264aefa00c2c9e8414bb408f254f
Carlos Rocha <crocha09.09@gmail.com>
2023-11-16 22:03:33 -0600

    show data from the API

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cinenica/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -9,7 +11,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Placeholder(),
+      body: _HomeView(),
     );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).getNextPage();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final moviesPlayingNow = ref.watch(nowPlayingMoviesProvider);
+    return ListView.builder(
+      itemCount: moviesPlayingNow.length,
+      itemBuilder: (context, index) {
+        final movie = moviesPlayingNow[index];
+        return ListTile(
+          title: Text(movie.title),
+          subtitle: Text(movie.overview),
+          leading: Image.network(movie.posterPath),
+        );
+      }
+      );
   }
 }
